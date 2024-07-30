1a0a656e184c97d058b9e9bc85f8e1078d21d457
Kajal Kokare <162293149+kokarekajal@users.noreply.github.com>
2024-07-30 15:49:51 +0530

    ch1_Update main.py

--- main.py
+++ main.py
@@ -47,5 +47,5 @@ if __name__ == '__main__':
   test_number_to_pair(5, 'White', 'Slate')
   test_pair_to_number('Black', 'Orange', 12)
   test_pair_to_number('Violet', 'Slate', 25)
-  test_pair_to_number('Red', 'Orange', 7)
+  #test_pair_to_number('Red', 'Orange', 7)
   print('Done :)')
