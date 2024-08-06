5fc011265a83b07ae76789a60baabebb27f70f85
Kajal Kokare <162293149+kokarekajal@users.noreply.github.com>
2024-08-06 14:39:27 +0530

    Update color_code.py

--- color_code.py
+++ color_code.py
@@ -9,12 +9,6 @@ def validate_pair_number(pair_number):
     if not (1 <= pair_number <= max_pair_number):
         raise ValueError('Pair number out of range')
 
-def validate_color(major_color, minor_color):
-    if major_color not in MAJOR_COLORS:
-        raise ValueError('Major color out of range')
-    if minor_color not in MINOR_COLORS:
-        raise ValueError('Minor color out of range')
-
 def get_color_from_pair_number(pair_number):
     validate_pair_number(pair_number)
     zero_based_pair_number = pair_number - 1
@@ -23,7 +17,8 @@ def get_color_from_pair_number(pair_number):
     return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
 
 def get_pair_number_from_color(major_color, minor_color):
-    validate_color(major_color, minor_color)
+    if major_color not in MAJOR_COLORS or minor_color not in MINOR_COLORS:
+        raise ValueError('Color out of range')
     major_index = MAJOR_COLORS.index(major_color)
     minor_index = MINOR_COLORS.index(minor_color)
     return major_index * len(MINOR_COLORS) + minor_index + 1
